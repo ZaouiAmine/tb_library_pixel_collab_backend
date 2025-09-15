e7ba0edb3de23091ffc6755af4bbed6216efaa66
Zaoui Amine <dev.zaoui@gmail.com>
2025-09-15 18:50:24 +0100

    -

--- backend.go
+++ backend.go
@@ -195,15 +195,25 @@ func getMessages(e event.Event) uint32 {
 
 //export onPixelUpdate
 func onPixelUpdate(e event.Event) uint32 {
+	fmt.Println("=== onPixelUpdate triggered ===")
+	fmt.Println("Event type:", fmt.Sprintf("%T", e))
+
 	channel, err := e.PubSub()
 	if err != nil {
+		fmt.Println("Error getting PubSub channel:", err)
 		return 1
 	}
 
 	data, err := channel.Data()
 	if err != nil {
+		fmt.Println("Error getting channel data:", err)
 		return 1
 	}
+
+	fmt.Println("Raw channel data type:", fmt.Sprintf("%T", data))
+	fmt.Println("Raw channel data length:", len(data))
+	fmt.Println("Raw channel data bytes:", data)
+	fmt.Println("Received pixel data as string:", string(data))
 
 	var pixelUpdate struct {
 		X        int    `json:"x"`
@@ -216,8 +226,12 @@ func onPixelUpdate(e event.Event) uint32 {
 
 	err = json.Unmarshal(data, &pixelUpdate)
 	if err != nil {
+		fmt.Println("Error parsing pixel data:", err)
 		return 1
 	}
+
+	fmt.Printf("Parsed pixel: x=%d, y=%d, color=%s, room=%s\n",
+		pixelUpdate.X, pixelUpdate.Y, pixelUpdate.Color, pixelUpdate.Room)
 
 	// Use room from message
 	room := pixelUpdate.Room
@@ -272,23 +286,39 @@ func onPixelUpdate(e event.Event) uint32 {
 		if err == nil {
 			pixelData, _ := json.Marshal(pixelUpdate)
 			pixelChannel.Publish(pixelData)
+			fmt.Println("Broadcasted pixel update to channel")
+		} else {
+			fmt.Println("Error getting pixel channel for broadcast:", err)
 		}
+	} else {
+		fmt.Println("Pixel coordinates out of bounds")
 	}
 
+	fmt.Println("=== onPixelUpdate completed ===")
 	return 0
 }
 
 //export onChatMessage
 func onChatMessage(e event.Event) uint32 {
+	fmt.Println("=== onChatMessage triggered ===")
+	fmt.Println("Event type:", fmt.Sprintf("%T", e))
+
 	channel, err := e.PubSub()
 	if err != nil {
+		fmt.Println("Error getting PubSub channel:", err)
 		return 1
 	}
 
 	data, err := channel.Data()
 	if err != nil {
+		fmt.Println("Error getting channel data:", err)
 		return 1
 	}
+
+	fmt.Println("Raw chat channel data type:", fmt.Sprintf("%T", data))
+	fmt.Println("Raw chat channel data length:", len(data))
+	fmt.Println("Raw chat channel data bytes:", data)
+	fmt.Println("Received chat data as string:", string(data))
 
 	var message struct {
 		Message  string `json:"message"`
